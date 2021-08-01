1a91e3c15e929c2c841bbdbb5136af7250c0ef6b
SergeyLepeshinsky <thsname18@gmail.com>
2021-08-01 13:24:59 +0300

    add console.writeline

--- SoftwareDevelopConsole/Program.cs
+++ SoftwareDevelopConsole/Program.cs
@@ -7,6 +7,7 @@ namespace SoftwareDevelopConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Привет. ");
         }
     }
 }
